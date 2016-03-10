c3614ba51f419eaff611eed2ae3affc61109b8f8
thomas.schneider <dolucky@ya.ru>
2016-03-10 19:08:55 +0300

    fd

--- simple-app/simple.js
+++ simple-app/simple.js
@@ -1,8 +1,23 @@
 var http = require('http');
 
+var request = require("request");
+
+request({
+  uri: "http://10.67.79.3:8080/sample",
+  method: "GET",
+  timeout: 10000,
+  followRedirect: true,
+  maxRedirects: 10
+}, function(error, response, body) {
+  console.log(body);
+});
+
+
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('Hello Http');
+  res.end(request);
 });
+
+
 
 server.listen(3000);
