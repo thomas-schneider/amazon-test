ff5a98ee20e5f5406ae13333985c52df478febae
thomas.schneider <dolucky@ya.ru>
2016-03-18 20:45:51 +0300

    asd

--- simple-app/simple.js
+++ simple-app/simple.js
@@ -2,22 +2,23 @@ var http = require('http');
 
 var request = require("request");
 
-request({
-  uri: "http://localhost/sample",
-  method: "GET",
-  timeout: 10000,
-  followRedirect: true,
-  maxRedirects: 10
-}, function(error, response, body) {
-  console.log(body);
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+
+    request({
+        uri: 'http://10.67.79.71:8080/sample',
+        method: 'GET',
+        maxRedirects:3
+    }, function(error, response, body) {
+        if (!error) {
+        	res.write(body)
+            res.write(response.statusCode);
+        } else {
+            //response.end(error);
+            res.write(error);
+        }
+    });     
+
+    res.end();
 });
-
-
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end(request);
-});
-
-
-
 server.listen(3000);
